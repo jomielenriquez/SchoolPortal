838f7808fb62efc3f90772edba238c8f49eaa1a2
Jomiel <enriquez.jliquigan@gmail.com>
2024-09-21 20:42:39 +0800

    Add project files.

---
+++ Portal/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Portal.Models;
+using System.Diagnostics;
+
+namespace Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult UnderConstruction()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
